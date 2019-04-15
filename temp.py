bce08ce0c448b2d6fd9c38303dcae2a831383cd0
Suhyun Kim <logicdevildotcom@gmail.com>
2019-04-14 22:41:14 -0400

    modified temp

--- temp.py
+++ temp.py
@@ -1,15 +1,11 @@
 import subprocess
-import numpy as np
 import os.path
 import json
 
-# TODO: end it with the slash
-ORIGINAL_PATH = '/Users/suhyunkim/git/DnntalPrivate/img/mask/'
-# TODO: end it with the slash
-RETRO_PATH = '/Users/suhyunkim/git/DnntalPrivate/img/radio/'
-
-is_diving = True
-
+#TODO: don't end it with the slash
+test_loc = "/home/kim/git/hw-grading-scripts/hw5/user/test"
+test_loc2 = "/home/kim/git/hw-grading-scripts/hw5/user/test/azurill-p"
+team_name = 'azurill'
 
 def run_command(command, logfile=None, print_output=True, return_output=True):
     # if logfile != None:
@@ -20,22 +16,41 @@ def run_command(command, logfile=None, print_output=True, return_output=True):
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
         executable='/bin/bash'
-    ).stdout.read()
+    ).stdout.read().decode('utf-8').strip()
     if print_output:
         print(output)
     if return_output:
-        return str(output)
+        return output
 
 
 # move the pictures to a certain directory and create labels
-arr_picture = np.array([])
-arr_total_score = np.array([])
-arr_difficulty_score = np.array([])
 
 
 def do():
-    result = run_command("cat /home/kim/git/hw-grading-scripts/hw5/user/test/azurill-p3-test/Makefile | grep TESTS")
-    print(result)
+    leaks = []
+    for i in range(1, 5):
+        run_command("git checkout -f hw5p%dhandin" %i)
+        run_command("cd user/module/fridge")
+        run_command("make")
+        run_command("sudo /usr/local/bin/kedr start fridge.ko")
+        run_command("sudo insmod fridge.ko")
+        run_command("cd %s/%s-p%d-test" %(test_loc, team_name, i))
+        run_command("make")
 
+
+        result = run_command("cat %s%d-test/Makefile | grep \"TESTS =\"" %(test_loc2, i))
+        result = result.split()
+        result = result[2:]
+        print(result)
+        for test in result:
+            run_command('./%s' %test)
+
+        run_command("sudo rmmod fridge")
+        run_command("make clean")
+        run_command("sudo /usr/local/bin/kedr stop")
+        result1 = run_command("sudo dmesg | grep \"Possible leaks\"")
+        leaks.append(result1)
+
+    print(leaks)
 
 do()
