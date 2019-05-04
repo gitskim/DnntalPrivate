73f8f72301afa961f2ab501c11c6392fb036d00c
Suhyun Kim <logicdevildotcom@gmail.com>
2019-05-04 15:42:26 -0400

    preprocessing

--- train.py
+++ train.py
@@ -2,7 +2,19 @@ import os
 import numpy as np
 import zipfile
 import subprocess
+# For building the model_2
+import tensorflow as tf
+import keras as keras
+from keras.models import Model, load_model
+from keras.layers import Input
+from keras.layers.core import Lambda, RepeatVector, Reshape
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras import backend as K
 
+PATH_HOME = '/home/ek2993/dnntal/DnntalPrivate/dnntal/dentist_AI'
 # --- get data ---
 def run_command(command, logfile=None, print_output=True, return_output=True):
     # if logfile != None:
@@ -19,37 +31,28 @@ def run_command(command, logfile=None, print_output=True, return_output=True):
     if return_output:
         return str(output)
 
-run_command("wget https://storage.googleapis.com/dentist_ai/dentist_AI.zip\
-    -O ./dnntal.zip")
-
-local_zip = '/home/ek2993/dnntal/DnntalPrivate/dnntal.zip'
-zip_ref = zipfile.ZipFile('/home/ek2993/dnntal/DnntalPrivate/dnntal.zip', 'r')
-
-zip_ref.extractall('/home/ek2993/dnntal/DnntalPrivate/dnntal')
-
-zip_ref.close()
-
-'''
-for path_original in filelist_original:
-  clahe(path_original)
-for path_mask in filelist_original:
-  clahe(path_mask)
-
-
+# Preprocessing
 im_width = 128
 im_height = 128
 border = 5
 im_chan = 2 # Number of channels: first is original and second cumsum(axis=0)
  
-filelist_original = glob.glob(os.path.join('/home/ek2993/dnntal/train/original/', '*.jpg'))
+filelist_original = glob.glob(os.path.join('/home/ek2993/dnntal/DnntalPrivate/dnntal/dentist_AI/train/original/', '*.jpg'))
 filelist_original=sorted(filelist_original)
-filelist_masks = glob.glob(os.path.join('/home/ek2993/dnntal/train/masks/', '*.jpg'))
+filelist_masks = glob.glob(os.path.join('/home/ek2993/dnntal/DnntalPrivate/dnntal/dentist_AI/train/masks/', '*.jpg'))
 filelist_masks=sorted(filelist_masks)
-'''
 
 resolution=2.0
 
+def clahe(path):
+  clahe = cv2.createCLAHE(clipLimit=resolution, tileGridSize=(16,16))
+  img=cv2.imread(path, 0)
+  cv2.imwrite(path, clahe.apply(img))
 
 train_path = '/home/suhyunkim011/dentist_AI/dentist_AI'
 
 
+for path_original in filelist_original:
+  clahe(path_original)
+for path_mask in filelist_original:
+  clahe(path_mask)
