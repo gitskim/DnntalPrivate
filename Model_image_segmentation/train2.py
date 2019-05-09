83264c2ae5b1b1c47f57e8da47e9f001b570f837
Suhyun Kim <logicdevildotcom@gmail.com>
2019-05-08 22:12:23 -0400

    added train2

--- Model_image_segmentation/train2.py
+++ Model_image_segmentation/train2.py
@@ -248,6 +248,25 @@ def unet(opt, input_size, lossfxn):
     model.compile(optimizer=opt, loss=lossfxn, metrics=[dsc, tp, tn])
     return model
 
+# explanation: https://software.intel.com/en-us/articles/hands-on-ai-part-14-image-data-preprocessing-and-augmentation
+datagen = ImageDataGenerator(
+    # to normalize the dataset such that the mean value of each data sample would be equal to 0
+    featurewise_center=True,
+    # sets the standard deviation value to 1.
+    featurewise_std_normalization=True,
+    shear_range=0.75,
+    zoom_range=0.5,
+    horizontal_flip=Ture,
+    fill_mode="nearest"
+)
+
+# compute quantities required for featurewise normalization
+# (std, mean, and principal components if ZCA whitening is applied)
+datagen.fit(X)
+
+# fits the model on batches with real-time data augmentation:
+model.fit_generator(datagen.flow(X, y, batch_size=32),
+                    steps_per_epoch=len(x_train) / 32, epochs=epochs)
 
 img_row = 128
 img_col = 128
@@ -263,3 +282,44 @@ hist = model.fit(X, y, validation_split=0.15,
                  verbose=True)
 
 # results = model.fit({'img': X_train, 'feat': X_feat_train}, y_train, batch_size=16, epochs=50, callbacks=callbacks)
+
+# Function to distort image
+def elastic_transform(image, alpha, sigma, alpha_affine, random_state=None):
+    """Elastic deformation of images as described in [Simard2003]_ (with modifications).
+    .. [Simard2003] Simard, Steinkraus and Platt, "Best Practices for
+         Convolutional Neural Networks applied to Visual Document Analysis", in
+         Proc. of the International Conference on Document Analysis and
+         Recognition, 2003.
+
+     Based on https://gist.github.com/erniejunior/601cdf56d2b424757de5
+    """
+    if random_state is None:
+        random_state = np.random.RandomState(None)
+
+    shape = image.shape
+    shape_size = shape[:2]
+
+    # Random affine
+    center_square = np.float32(shape_size) // 2
+    square_size = min(shape_size) // 3
+    pts1 = np.float32([center_square + square_size, [center_square[0] + square_size, center_square[1] - square_size],
+                       center_square - square_size])
+    pts2 = pts1 + random_state.uniform(-alpha_affine, alpha_affine, size=pts1.shape).astype(np.float32)
+    M = cv2.getAffineTransform(pts1, pts2)
+    image = cv2.warpAffine(image, M, shape_size[::-1], borderMode=cv2.BORDER_REFLECT_101)
+
+    dx = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma) * alpha
+    dy = gaussian_filter((random_state.rand(*shape) * 2 - 1), sigma) * alpha
+    dz = np.zeros_like(dx)
+
+    x, y, z = np.meshgrid(np.arange(shape[1]), np.arange(shape[0]), np.arange(shape[2]))
+    indices = np.reshape(y + dy, (-1, 1)), np.reshape(x + dx, (-1, 1)), np.reshape(z, (-1, 1))
+
+    return map_coordinates(image, indices, order=1, mode='reflect').reshape(shape)
+
+# Apply transformation on image
+im_merge_t = elastic_transform(im_merge, im_merge.shape[1] * 2, im_merge.shape[1] * 0.08, im_merge.shape[1] * 0.08)
+
+# Split image and mask
+im_t = im_merge_t[...,0]
+im_mask_t = im_merge_t[...,1]
