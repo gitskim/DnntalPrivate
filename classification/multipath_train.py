73dd27260cf9264fddbcd603323b922107a3b427
Suhyun Kim <logicdevildotcom@gmail.com>
2019-05-13 20:09:43 -0400

    first version of multipath

---
+++ classification/multipath_train.py
@@ -0,0 +1,276 @@
+import os
+import glob
+import h5py
+import shutil
+import imgaug as aug
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import imgaug.augmenters as iaa
+from os import listdir, makedirs, getcwd, remove
+from os.path import isfile, join, abspath, exists, isdir, expanduser
+from PIL import Image
+from pathlib import Path
+from skimage.io import imread
+from skimage.transform import resize
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.applications.vgg16 import VGG16, preprocess_input
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Input, Flatten, SeparableConv2D
+from tensorflow.python.keras.layers import GlobalMaxPooling2D
+from tensorflow.python.keras.layers.normalization import BatchNormalization
+from tensorflow.python.keras.layers.merge import Concatenate
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.optimizers import Adam, SGD, RMSprop
+from tensorflow.python.keras.callbacks import ModelCheckpoint, Callback, EarlyStopping
+from tensorflow.python.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+import cv2
+from keras import backend as K
+import json
+
+# Define path to the data directory
+data_dir = '/home/ek2993/DnntalPrivate/cropped'
+
+# Path to train directory
+train_dir = os.path.join(data_dir, 'train')
+
+# Path to validation directory
+validation_dir = os.path.join(data_dir, 'val')
+
+# Get the path to the positive and negative sub-directories
+train_positive_dir = os.path.join(train_dir, 'cropped_positive_xrays')
+train_negative_dir = os.path.join(train_dir, 'cropped_negative_xrays')
+validation_positive_dir = os.path.join(validation_dir, 'cropped_positive_xrays')
+validation_negative_dir = os.path.join(validation_dir, 'cropped_negative_xrays')
+
+# Get the list of all the images
+positives_cases_train = glob.glob(os.path.join(train_positive_dir, '*.jpg'))
+negatives_cases_train = glob.glob(os.path.join(train_negative_dir, '*.jpg'))
+positives_cases_val = glob.glob(os.path.join(validation_positive_dir, '*.jpg'))
+negatives_cases_val = glob.glob(os.path.join(validation_negative_dir, '*.jpg'))
+
+# An empty list. We will insert the data into this list in (img_path, label) format
+train_data = []
+
+# List that are going to contain validation images data and the corresponding labels
+valid_data = []
+valid_labels = []
+
+# Go through all the negatives cases. The label for these cases will be 0
+for img in negatives_cases_train:
+    train_data.append((img, 0))
+
+# Go through all the positives cases. The label for these cases will be 1
+for img in positives_cases_train:
+    train_data.append((img, 1))
+
+# Get a pandas dataframe from the data we have in our list
+train_data = pd.DataFrame(train_data, columns=['image', 'label'], index=None)
+
+# Shuffle the data
+train_data = train_data.sample(frac=1.).reset_index(drop=True)
+
+# We will convert into a image with 3 channels.
+# We will normalize the pixel values and resizing all the images to 224x224
+
+# Negatives cases
+for img in negatives_cases_train:
+    img = cv2.imread(str(img))
+    img = cv2.resize(img, (224, 224))
+    if img.shape[2] == 1:
+        img = np.dstack([img, img, img])
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img.astype(np.float32) / 255.
+    label = to_categorical(0, num_classes=2)
+    valid_data.append(img)
+    valid_labels.append(label)
+
+# Positives cases
+for img in positives_cases_train:
+    img = cv2.imread(str(img))
+    img = cv2.resize(img, (224, 224))
+    if img.shape[2] == 1:
+        img = np.dstack([img, img, img])
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img.astype(np.float32) / 255.
+    label = to_categorical(1, num_classes=2)
+    valid_data.append(img)
+    valid_labels.append(label)
+
+# Convert the list into numpy arrays
+valid_data = np.array(valid_data)
+valid_labels = np.array(valid_labels)
+
+print("Total number of validation examples: ", valid_data.shape)
+print("Total number of labels:", valid_labels.shape)
+
+# Augmentation sequence
+seq = iaa.OneOf([
+    iaa.Fliplr(),  # horizontal flips
+    iaa.Affine(rotate=20),  # roatation
+    iaa.Multiply((1.2, 1.5))])  # random brightness
+
+
+def data_gen(data, batch_size):
+    # Get total number of samples in the data
+    n = len(data)
+    steps = n // batch_size
+
+    # Define two numpy arrays for containing batch data and labels
+    batch_data = np.zeros((batch_size, 224, 224, 3), dtype=np.float32)
+    batch_labels = np.zeros((batch_size, 2), dtype=np.float32)
+
+    # Get a numpy array of all the indices of the input data
+    indices = np.arange(n)
+
+    # Initialize a counter
+    i = 0
+    while True:
+        np.random.shuffle(indices)
+        # Get the next batch
+        count = 0
+        next_batch = indices[(i * batch_size):(i + 1) * batch_size]
+        for j, idx in enumerate(next_batch):
+            img_name = data.iloc[idx]['image']
+            label = data.iloc[idx]['label']
+
+            # one hot encoding
+            encoded_label = to_categorical(label, num_classes=2)
+            # read the image and resize
+            img = cv2.imread(str(img_name))
+            img = cv2.resize(img, (224, 224))
+
+            # check if it's grayscale
+            if img.shape[2] == 1:
+                img = np.dstack([img, img, img])
+
+            # cv2 reads in BGR mode by default
+            orig_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            # normalize the image pixels
+            orig_img = img.astype(np.float32) / 255.
+
+            batch_data[count] = orig_img
+            batch_labels[count] = encoded_label
+
+            # generating more samples of the undersampled class
+            if label == 0 and count < batch_size - 2:
+                aug_img1 = seq.augment_image(img)
+                aug_img2 = seq.augment_image(img)
+                aug_img1 = cv2.cvtColor(aug_img1, cv2.COLOR_BGR2RGB)
+                aug_img2 = cv2.cvtColor(aug_img2, cv2.COLOR_BGR2RGB)
+                aug_img1 = aug_img1.astype(np.float32) / 255.
+                aug_img2 = aug_img2.astype(np.float32) / 255.
+
+                batch_data[count + 1] = aug_img1
+                batch_labels[count + 1] = encoded_label
+                batch_data[count + 2] = aug_img2
+                batch_labels[count + 2] = encoded_label
+                count += 2
+
+            else:
+                count += 1
+
+            if count == batch_size - 1:
+                break
+
+        i += 1
+        yield batch_data, batch_labels
+
+        if i >= steps:
+            i = 0
+
+
+class MyModel(Model):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.conv1 = Conv2D(32, 3, activation='relu')
+        self.flatten = Flatten()
+        self.d2 = Dense(10, activation='softmax')
+
+        self.two = Conv2D(32, 3, activation='relu')
+        self.thr = Flatten()
+        self.fiv = Dense(10, activation='softmax')
+
+        self.add = tf.keras.layers.Add()
+
+    def call(self, x, y):
+        x = self.conv1(x)
+        x = self.flatten(x)
+
+        y = self.two(y)
+        y = self.thr(y)
+        add = self.add([x, y])
+        return self.fiv(add)
+
+model = MyModel()
+
+# def build_model():
+#     input_img = Input(shape=(224, 224, 3), name='ImageInput')
+#     x = Conv2D(64, (3, 3), activation='relu', padding='same', name='Conv1_1')(input_img)
+#     x = Conv2D(64, (3, 3), activation='relu', padding='same', name='Conv1_2')(x)
+#     x = MaxPooling2D((2, 2), name='pool1')(x)
+#
+#     x = SeparableConv2D(128, (3, 3), activation='relu', padding='same', name='Conv2_1')(x)
+#     x = SeparableConv2D(128, (3, 3), activation='relu', padding='same', name='Conv2_2')(x)
+#     x = MaxPooling2D((2, 2), name='pool2')(x)
+#
+#     x = SeparableConv2D(256, (3, 3), activation='relu', padding='same', name='Conv3_1')(x)
+#     x = BatchNormalization(name='bn1')(x)
+#     x = SeparableConv2D(256, (3, 3), activation='relu', padding='same', name='Conv3_2')(x)
+#     x = BatchNormalization(name='bn2')(x)
+#     x = SeparableConv2D(256, (3, 3), activation='relu', padding='same', name='Conv3_3')(x)
+#     x = MaxPooling2D((2, 2), name='pool3')(x)
+#
+#     x = SeparableConv2D(512, (3, 3), activation='relu', padding='same', name='Conv4_1')(x)
+#     x = BatchNormalization(name='bn3')(x)
+#     x = SeparableConv2D(512, (3, 3), activation='relu', padding='same', name='Conv4_2')(x)
+#     x = BatchNormalization(name='bn4')(x)
+#     x = SeparableConv2D(512, (3, 3), activation='relu', padding='same', name='Conv4_3')(x)
+#     x = MaxPooling2D((2, 2), name='pool4')(x)
+#
+#     x = Flatten(name='flatten')(x)
+#     x = Dense(1024, activation='relu', name='fc1')(x)
+#     x = Dropout(0.7, name='dropout1')(x)
+#     x = Dense(512, activation='relu', name='fc2')(x)
+#     x = Dropout(0.5, name='dropout2')(x)
+#     x = Dense(2, activation='softmax', name='fc3')(x)
+#
+#     model = Model(inputs=input_img, outputs=x)
+#     return model
+
+
+# opt = RMSprop(lr=0.0001, decay=1e-6)
+opt = Adam(lr=0.0001, decay=1e-5)
+es = EarlyStopping(patience=5)
+chkpt = ModelCheckpoint(filepath='best_model_todate_5_13_7p', save_best_only=True, save_weights_only=True)
+model.compile(loss='binary_crossentropy', metrics=['accuracy'], optimizer=opt)
+
+batch_size = 16
+nb_epochs = 20
+
+# Get a train data generator
+train_data_gen = data_gen(data=train_data, batch_size=batch_size)
+
+# Define the number of training steps
+nb_train_steps = train_data.shape[0] // batch_size
+
+print("Number of training and validation steps: {} and {}".format(nb_train_steps, len(valid_data)))
+
+# Fit the model
+history = model.fit_generator(train_data_gen, epochs=nb_epochs, steps_per_epoch=nb_train_steps,
+                              validation_data=(valid_data, valid_labels), callbacks=[es, chkpt],
+                              class_weight={0: 1.0, 1: 0.4})
+
+# serialize model to JSON
+model_json = model.to_json()
+with open("model_5_10_8p.json", "w") as json_file:
+    json_file.write(model_json)
+
+# serialize weights to HDF5
+model.save_weights('weights_5_13_7p.h')
+print(history.history)
+with open('history_5_13_7p.json', 'w') as f:
+    json.dump(float(history.history), f)
