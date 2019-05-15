23053fff083ee7c7678110d04782101a1af4ba8c
Suhyun Kim <logicdevildotcom@gmail.com>
2019-05-15 19:32:26 +0000

    added graph

--- image_segmentation/graph.py
+++ image_segmentation/graph.py
@@ -1,7 +1,16 @@
 import json
-
+'''
+val_tp
+val_tn
+val_loss
+val_dsc
+dsc
+tn
+tp
+loss
+'''
 with open('seg_5_15_7a.json', 'r') as f:
     json_obj = json.load(f)
     for obj in json_obj:
-        print(obj) #key
-        # print(json_obj[obj]) val
+        # print(obj) #key
+        print(json_obj['val_dsc']) #val
